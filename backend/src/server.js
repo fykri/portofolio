ee3367d79220f8b948c7a61446e48ecccb7ca86d
fykri <dzulfikriyunus@gmail.com>
2026-09-05 23:50:01 +0800

    add: port in server backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT || 3000, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
