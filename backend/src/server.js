f22e890b06c4bac7d31fae6082926d41acfcb4fa
fykri <dzulfikriyunus@gmail.com>
2026-09-05 23:50:28 +0800

    eads

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT || 3000, () => {
+app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
